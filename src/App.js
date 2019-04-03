1cc9e2969d30b1841fcafe4739694050ab2bcdb9
Clint Ryan <c.ryan@nib.com.au>
2019-04-03 19:15:38 +1100

    Added middleware and some more basic

--- src/App.js
+++ src/App.js
@@ -7,15 +7,29 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'friend'
+      name: 'friend',
+      isHidden: true
     };
   }
 
   async componentDidMount() {
-    const response = await fetch('http://localhost:9000/api/v1/hello');
+    const id = window.localStorage.getItem('id');
+    // this is just demo code, please don't do things this way
+    const options = {};
+    if (id) {
+      options.headers = {
+        "x-user-id": id
+      }
+    }
+    const response = await fetch('http://localhost:9000/api/v1/hello', options);
+
     const msg = await response.json();
+    const userId = msg.id;
+    if (userId !== id) window.localStorage.setItem('id', userId);
     this.setState({
-      name: msg
+      name: msg.name,
+      id: userId,
+      experiment: msg.experiment
     });
   }
 
@@ -23,17 +37,29 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
         </header>
         <div className="App-container">
           <h1 style={{textAlign: "center"}}>Welcome to the nib optimizely training {this.state.name}!</h1>
           <div className="page-container">
             <h2>First Step</h2>
             <hr/>
-            <p>The above name is generated from the API. The SDK is installed on the API serving the content. See if you can change the messaging and/or font-colour based on a variation.</p>
+            <p>The above name is generated from the API. The SDK is installed on the API serving the content. See if you can get the SDK connected on the server to optimizely.</p>
             <h2>Second Step</h2>
             <hr/>
-            <p>Test</p>
+            <p>Your user id is <strong>{this.state.id ? this.state.id : '...'}</strong>, this is auto-generated on the server and saved in local storage on the browser. It is used to track your optimizely session and uniquely identify a user and their variation. Try clear your local storage to update it.</p>
+            <p>When using optimizely full stack, it's our job to make sure that we keep track of the unique id of the user throughout the experience. Ideally, the user experience should be consistent, so make sure that the variation does not change for any particular userId. Also keep in mind that this user id is a naive number between 0 -> 1000</p>
+            <h2>Third Step</h2>
+            <hr/>
+            <p>I have retrieved some statically defined variation data from the server, this looks like the following:</p>
+            <code>
+              {this.state.experiment ? JSON.stringify(this.state.experiment) : 'Loading...'}
+            </code>
+            <p>Using optimizely, create some new variations and try and get the optimizely SDK returning some more variation data to the frontend. Examples include:</p>
+            <ul>
+              <li>Change icon at the top</li>
+              <li>Create a hero image</li>
+            </ul>
           </div>
         </div>
       </div>
--- src/server.js
+++ src/server.js
@@ -1,15 +1,62 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const optimizely = require('@optimizely/optimizely-sdk');
 const app = express();
 
-app.use(cors());
+// Instantiate an Optimizely client
+// const datafile = null;
+// const optimizelyClientInstance = optimizely.createInstance({ datafile: datafile });
 
+const userMiddleware = (req, res, next) => {
+  let userId = req.headers['x-user-id'];
+  console.log(userId);
+  if (!userId) {
+    userId = Math.floor(Math.random() * 1000);
+  }
+  req.user = {
+    id: userId
+  }
+  next();
+}
+
+
+const variationMiddleware = (req, res, next) => {
+
+  const enabled = false;
+  const min_price = 10;
+  const variation = 'control';
+
+  // const variation = optimizelyClientInstance.activate('experiment', userId); // we pay for this call
+  // Evaluate a feature flag and a variable
+  // const enabled = optimizelyClientInstance.isFeatureEnabled('price_filter', userId);
+  // const min_price = optimizelyClientInstance.getFeatureVariableInteger('price_filter', 'min_price', userId);
+
+  req.user.optimizely = {
+    enabled,
+    min_price,
+    variation
+  }
+
+  next();
+}
+
+const optimizelyMiddleware = [userMiddleware, variationMiddleware];
+
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/api/v1/hello', (req, res) => {
+app.get('/api/v1/hello', optimizelyMiddleware, (req, res) => {
+  const userId = req.user.id;
+  const experiment = req.user.optimizely;
+
   setTimeout(() => {
-    res.status(200).json('James');
+    // optimizelyClientInstance.track('purchased', userId);
+    res.status(200).json({
+      name: 'James',
+      id: userId,
+      experiment
+    });
   }, 1000)
 });
 
